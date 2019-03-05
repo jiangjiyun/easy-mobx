1b98ede60df6255590c1d62a545558098f5182cd
Jiangjiyun <Jiangjiyun@chelun.com>
2019-03-05 15:02:12 +0800

    添加react支持

--- src/my-autorun.js
+++ src/my-autorun.js
@@ -1,7 +1,26 @@
 import dependenceManger from './my-dependence-manager';
 
-export default function autorun(handle){
+export default function autorun(handle) {
   dependenceManger.startCollect(handle);
   handle();
   dependenceManger.endCollect();
+}
+
+
+//对autorun进行封装处理，支持react
+const ReactMixin = {
+  componentWillMount: function () {
+    autorun(() => {
+      this.render();
+      this.forceUpdate();
+    });
+  }
+};
+
+export function observer(target) {
+  const targetCWM = target.prototype.componentWillMount;
+  target.prototype.componentWillMount = function () {
+    targetCWM && targetCWM.call(this);
+    ReactMixin.componentWillMount.call(this);
+  };
 }
